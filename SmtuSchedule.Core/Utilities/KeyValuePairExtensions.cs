fd01b94fb8bdaab8a71359512cd73e71a7491e01
Svyatoslav Shults <svsch9@yandex.ru>
2019-11-15 17:10:03 +0300

    Renamed KeyValuePair extensions class to match the overall style

--- SmtuSchedule.Core/Utilities/KeyValuePairExtensions.cs
+++ SmtuSchedule.Core/Utilities/KeyValuePairExtensions.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace SmtuSchedule.Core.Utilities
 {
-    internal static class KeyValuePairExtension
+    internal static class KeyValuePairExtensions
     {
         public static void Deconstruct<T1, T2>(this KeyValuePair<T1, T2> tuple, out T1 key, out T2 value)
         {
