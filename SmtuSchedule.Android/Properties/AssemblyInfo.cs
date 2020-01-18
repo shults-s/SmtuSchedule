67750d108e07be6463b23de44edd513123f8e76d
Svyatoslav Shults <svsch9@yandex.ru>
2020-01-18 20:35:00 +0300

    Updated year in the copyright of the assembly

--- SmtuSchedule.Android/Properties/AssemblyInfo.cs
+++ SmtuSchedule.Android/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyProduct("SmtuSchedule")]
-[assembly: AssemblyCopyright("Copyright © 2019 Svyatoslav Shults")]
+[assembly: AssemblyCopyright("Copyright © 2020 Svyatoslav Shults")]
