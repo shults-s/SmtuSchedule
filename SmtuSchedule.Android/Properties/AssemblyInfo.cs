c20c8d80ec9d1d008fcb430fb5c0ba8a0098a716
Svyatoslav Shults <svsch9@yandex.ru>
2020-02-16 17:30:41 +0300

    Minor improvement of assembly version in the SmtuSchedule.Android project

--- SmtuSchedule.Android/Properties/AssemblyInfo.cs
+++ SmtuSchedule.Android/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyProduct("SmtuSchedule")]
 [assembly: AssemblyCopyright("Copyright © 2020 Svyatoslav Shults")]
