cb29b58657de2ffe88936e4ad2522ba808a79967
Svyatoslav Shults <svsch9@yandex.ru>
2020-03-31 17:50:06 +0300

    Minor performance improvements in the KeyValuePairExtensions class

--- SmtuSchedule.Core/Utilities/KeyValuePairExtensions.cs
+++ SmtuSchedule.Core/Utilities/KeyValuePairExtensions.cs
@@ -4,7 +4,7 @@ namespace SmtuSchedule.Core.Utilities
 {
     internal static class KeyValuePairExtensions
     {
-        public static void Deconstruct<T1, T2>(this KeyValuePair<T1, T2> tuple, out T1 key, out T2 value)
+        public static void Deconstruct<T1, T2>(this in KeyValuePair<T1, T2> tuple, out T1 key, out T2 value)
         {
             key = tuple.Key;
             value = tuple.Value;
