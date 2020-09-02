1effc65143c701c3271a4725fc1d0248c9b27260
Svyatoslav Shults <svsch9@yandex.ru>
2020-09-03 01:08:08 +0300

    Added logging of a non-reproducible in vitro bug leading to a crash

--- SmtuSchedule.Android/Views/MainActivity.cs
+++ SmtuSchedule.Android/Views/MainActivity.cs
@@ -22,6 +22,7 @@ using SmtuSchedule.Core.Utilities;
 using SmtuSchedule.Core.Enumerations;
 using SmtuSchedule.Android.Utilities;
 using SmtuSchedule.Android.Interfaces;
+using SmtuSchedule.Android.Exceptions;
 using SmtuSchedule.Android.Enumerations;
 
 using PopupMenu = Android.Support.V7.Widget.PopupMenu;
@@ -563,6 +564,19 @@ namespace SmtuSchedule.Android.Views
             }
 
             IReadOnlyDictionary<Int32, Schedule> schedules = _application.Manager.Schedules;
+
+            // Невоспроизводимая искусственно ситуация, которая хоть и очень редко, но роняет приложение.
+            if (schedules == null)
+            {
+                UiException exception = new UiException("Unexpected null value of Schedules property.");
+                exception.Data["_stateManager.CurrentState"] = _stateManager.CurrentState;
+                exception.Data["_application.IsInitialized"] = _application.IsInitialized;
+                _application.Logger.Log(exception);
+
+                ShowLayoutMessage(Resource.String.restartSchedulesRenderingSubsystemErrorMessage, true);
+
+                return ;
+            }
 
             IReadOnlyList<Schedule> sortedSchedules = schedules.Select(s => s.Value)
                 .OrderBy(s => s, new SchedulesComparer())
